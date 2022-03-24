40fe9e39455e1a05207dce6626e4f8a5d5f55299
kenidy <kenidys@gmailcom>
2022-03-24 16:25:46 -0300

    First commit

---
+++ seeds.rb
@@ -0,0 +1,6 @@
+Country.create({"name"=>"Teste", "population"=>300183})
+Country.create({"name"=>"Teste", "population"=>300183})
+Country.create({"name"=>"Germany", "population"=>818355})
+Country.create({"name"=>"France", "population"=>818355})
+Country.create({"name"=>"Belgin", "population"=>818355})
+Country.create({"name"=>"Netherlands", "population"=>818355})
